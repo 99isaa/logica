a1a8067cbaf074bfb496decf6fcd83238923e79e
isinha <isa.alcantara2@gmail.com>
2018-04-19 12:32:51 -0300

    hoje

---
+++ jogo.js
@@ -0,0 +1,29 @@
+let entrada = document.querySelector("input");
+let botao = document.querySelector("button");
+let ana = 0;
+let marcos=0;
+
+botao.onclick = function(){
+    let palavra = entrada.value;
+    for (let test of palavra){
+        if(test === "a" || test === "e"|| test === "i"|| test === "o"|| test === "u"){  
+            ana++;
+        } else{
+            marcos++;
+        }
+    }
+    console.log (ana);
+    console.log (marcos);
+    if (ana > marcos){
+        let resultado = document.createElement("p");
+        resultado.innerHTML = "Ana Ganhou";
+        let body = document.querySelector ("body");
+        body.appendChild (resultado);
+    } 
+    else {
+        let resultado = document.createElement("p");
+        resultado.innerHTML = "Marcos Ganhou";
+        let body = document.querySelector ("body");
+        body.appendChild (resultado); 
+    }
+}
